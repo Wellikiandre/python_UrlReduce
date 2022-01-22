6a5d3282a317a15cbba14a3e08a18b8af9e23c7b
wellikiandre <wellikiandre@hotmail.com>
2022-01-22 20:08:53 -0300

    a resolver falaha UrlRedirect

--- projeto/encurtador/models.py
+++ projeto/encurtador/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class UrlRedirect(models.Model):
     destino = models.URLField(max_length=512)
     slug = models.SlugField(max_length=128, unique=True)
