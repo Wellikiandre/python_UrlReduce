70e749c950bb01eb5946a4e8c57adadf270410de
wellikiandre <wellikiandre@hotmail.com>
2022-01-22 19:48:25 -0300

    tela admin criada

--- projeto/encurtador/views.py
+++ projeto/encurtador/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+def redirecionar(requisicao,slug):
+    return redirect ('http://google.com')
+
+
--- projeto/urls.py
+++ projeto/urls.py
@@ -1,4 +1,4 @@
-"""devwellik URL Configuration
+"""projeto URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from devwellik.projeto.encurtador import views
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
+    path('<slug:slug>', views.redirecionar),
     path('admin/', admin.site.urls),
 ]
